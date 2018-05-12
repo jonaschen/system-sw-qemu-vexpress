5af6b292d7c76f68d60239c1c9b8f7ae5fad2509
jonaschen <genty921@gmail.com>
2018-05-12 15:42:09 +0800

    Initialize bss and data sections

--- README.md
+++ README.md
@@ -1,10 +1,18 @@
 # system-sw-qemu-vexpress
+---------
+This is a study project, just for fun.
+All the source code is based on jserv's examples.
+I have been studing jserv's tutorial materials and code samples for a while.
+To express my thankfulness, I am trying to give some feedback 
+and sharing of my experience.
+
 
 Licensing
 ---------
 `system-sw-qemu-vexpress` is freely redistributable under the two-clause BSD License. Use of this source code is governed by a BSD-style license that can be found in the `LICENSE` file.
 
 # Reference
+---------
 jserv's mini-arm-os - https://github.com/jserv/mini-arm-os
 
 jserv's Hello World for bare metal ARMv8 using QEMU
--- main.c
+++ main.c
@@ -1,4 +1,7 @@
+#include <stdint.h>
+
 #include "reg.h"
+
 
 int puts(const char *str)
 {
@@ -7,9 +10,61 @@ int puts(const char *str)
 	return 0;
 }
 
+/* start address for the initialization values of the .data section.
+defined in linker script */
+extern uint32_t _sidata;
+/* start address for the .data section. defined in linker script */
+extern uint32_t _sdata;
+/* end address for the .data section. defined in linker script */
+extern uint32_t _edata;
+/* start address for the .bss section. defined in linker script */
+extern uint32_t _sbss;
+/* end address for the .bss section. defined in linker script */
+extern uint32_t _ebss;
+/* end address for the stack. defined in linker script */
+extern uint32_t _estack;
+
+
+int data_test = 1;
+int bss_test;
+char bss_test2;
+char data_test2 = 2;
+
+static int exec_env_init(void)
+{
+	/* Copy the data segment initializers from flash to SRAM */
+	uint32_t *idata_begin = &_sidata;
+	uint32_t *data_begin = &_sdata;
+	uint32_t *data_end = &_edata;
+
+	if (idata_begin != data_begin) {
+		/* init data section */
+		while (data_begin < data_end) *data_begin++ = *idata_begin++;
+	}
+
+	/* Zero fill the bss segment. */
+	uint32_t *bss_begin = &_sbss;
+	uint32_t *bss_end = &_ebss;
+	while (bss_begin < bss_end) *bss_begin++ = 0;
+
+	if (data_test != 1 || data_test2 != 2)
+		return -1;
+
+	if (bss_test != 0 || bss_test2 != 0)
+		return -2;
+
+	return 0;
+}
+
 void main(void)
 {
+	if (exec_env_init()) {
+		puts("env init fail\n");
+		goto idle;
+	}
+
 	puts("Hello, Jonas.\n");
 
+idle:
 	while (1);
 }
